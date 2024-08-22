eb7f5def7bee756b9dc174b741fb82b874825390
Keith Donegan <keith.donegan@gmail.com>
2024-08-22 21:53:42 +0100

    updates

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const menuItems = document.querySelectorAll('#menu-primary .has-children > a');
 
     menuItems.forEach(item => {
-        item.addEventListener('click', function(event) {
-            event.preventDefault();
-            const submenu = this.nextElementSibling;
-            submenu.classList.toggle('open');
+        item.addEventListener('hover', function(event) {
+            // event.preventDefault();
+            // const submenu = this.nextElementSibling;
+            // submenu.classList.toggle('open');
         });
     });
 });
