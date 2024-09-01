33c9b7dfd89e76e865b8d009c092c4c6019e1487
Keith Donegan <keith.donegan@gmail.com>
2024-09-01 15:34:00 +0100

    bulk

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
+    
     const menuItemsWithChildren = document.querySelectorAll('#menu-primary .has-children');
 
-    if (window.matchMedia('(hover: hover)').matches) {
+    const primaryNav = document.getElementById('nav-primary');
+    const hamburger = document.getElementById('hamburger');
 
-        
+    hamburger.addEventListener('click', function(event) {
+        hamburger.classList.toggle('is-active');
+        primaryNav.classList.toggle('is-open');
+    });
 
-        menuItemsWithChildren.forEach(item => {
-            item.addEventListener('mouseenter', function(event) {
+    menuItemsWithChildren.forEach(parentLi => {
 
-                console.log('clicky');
-
-                item.classList.add('is-active');
-            });
-
-            item.addEventListener('mouseleave', function(event) {
-                item.classList.remove('is-active');
-            });
-
-            /* item.addEventListener('click', function(event) {
-                item.classList.toggle('clicked');
-            }); */
+        parentLi.addEventListener('click', function(event) {
+            parentLi.classList.toggle('is-open');
         });
-    }
+
+        /* parentLi.addEventListener('mouseenter', function(event) {
+            console.log('clicky');
+            parentLi.classList.add('is-open');
+        });
+        parentLi.addEventListener('mouseleave', function(event) {
+            parentLi.classList.remove('is-active');
+        }); */
+
+            
+    });
+    
 });
